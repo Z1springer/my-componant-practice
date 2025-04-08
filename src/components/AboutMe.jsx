e27852772d4db82d1fce2603b252d1fb7c90f3f5
Z1springer <zdspringer99@gmail.com>
2025-04-08 11:43:49 -0700

    Edited components and added self image

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,7 +1,10 @@
+import selfImg from "../assets/Self_portrait.jpeg";
+
 function About() {
     return (
         <>
             <h1>Name: Zachary Springer</h1>
+            <img src={selfImg}></img>
             <h2>Occupation: Software Development Student</h2>
             <h2>School: Greenriver College</h2>
         </>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,6 @@ function Contact() {
             <h3>Contact Information</h3>
             <p>Email: thisisafakeemail@gewgle.com</p>
             <p>Cell #: 123-456-7891</p>
-            <a>LinkedIn Site: </a>
         </>
     )
 };
